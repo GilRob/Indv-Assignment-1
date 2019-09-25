89d73291e7d70cf88a226f069cb3ada2359b3283
Gil <gil.robern@ontariotechu.net>
2019-09-25 14:56:10 -0400

    Start
    Took Unity and C++ project from last tutorial

---
+++ Tutorial2/Tutorial2/Wrapper.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "PluginSettings.h"
+#include "SimpleClass.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	// Put your functions here
+	PLUGIN_API int SimpleFunction();
+
+#ifdef __cplusplus
+}
+#endif
